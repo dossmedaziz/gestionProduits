a2737810f1e40521fde88b3871cd0ce11f427c44
dossmedaziz <dossaziz4@gmail.com>
2023-05-13 16:10:44 +0100

    delete products²

--- src/main/java/com/helloSpringBoot/gestionProduits/controllers/ProductController.java
+++ src/main/java/com/helloSpringBoot/gestionProduits/controllers/ProductController.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -15,10 +17,15 @@ public class ProductController {
     IServiceProduct sp;
 
     @GetMapping("/home")
-    public String getAll(Model m) {
-        m.addAttribute("products", sp.getAllProducts());
+    public String getAll(@RequestParam(name = "mc", defaultValue = "") String mc, Model m) {
+        m.addAttribute("products", sp.getProductsByMC(mc));
+        m.addAttribute("keyWord", mc);
         return "products";
+    }
 
-
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable Integer id) {
+        sp.deleteProduct(id);
+        return "redirect:/home";
     }
 }
